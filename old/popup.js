884858075293726695758cd2462674ae86bc7a7e
Andrejs Kovtuns <andkovt@gmail.com>
2023-03-05 14:09:03 +0200

    Removing old code

--- old/popup.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.session.get(["enabled"]).then((result => {
-    document.getElementById('enabled').checked = result.enabled;
-}));
-
-document.getElementById('enabled').addEventListener('change', (event) => {
-    chrome.storage.session.set({enabled: event.currentTarget.checked});
-})
