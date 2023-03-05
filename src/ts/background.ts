d897d178d9893198d4ecb6368771e2f7e07d7694
Andrejs Kovtuns <andkovt@gmail.com>
2023-03-05 14:08:40 +0200

    Implementing connection and refresh

---
+++ src/ts/background.ts
@@ -0,0 +1,46 @@
+import { Message } from "./ts/types";
+
+export {};
+
+let ws: WebSocket | null = null;;
+
+chrome.runtime.onMessage.addListener((message: Message, sender, sendResponse) => {
+    console.log(message.enabled);
+
+    if (message.enabled) {
+        openAndWatch();
+    } else {
+        close();
+    }
+});
+
+function close(): void {
+    if (ws !== null) {
+        ws.close();
+        ws = null;
+    }
+}
+
+function openAndWatch(): void {
+    close();
+
+    ws = new WebSocket("ws://127.0.0.1:9797/watch");
+
+    ws.onopen = () => {
+        console.log('Connected');
+        chrome.runtime.sendMessage({enabled: true, status: 'Connected'});
+    }
+    
+    ws.onmessage = (data) => {
+        chrome.tabs.reload();
+    }
+    
+    ws.onclose = (data) => {
+        chrome.runtime.sendMessage({enabled: false, status: 'Disconnected'});
+    }
+
+    ws.onerror = (event) => {
+        console.log(event);
+        chrome.runtime.sendMessage({enabled: false, status: 'Unable to connect to warb'});
+    }
+}
--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,8 +1,38 @@
 import '../scss/style.scss'
+import {Message} from './types';
 
 export {};
 
-const element = document.getElementById('wow');
-if (element !== null) {
-    element.innerText = 'Hello World';
+const enabledSwitch = document.getElementById('enabled-switch') as HTMLInputElement;
+const statusText = document.getElementById('status');
+
+const storage = await chrome.storage.session.get(['enabled', 'status']);
+
+if (storage.enabled && enabledSwitch !== null) {
+    enabledSwitch.checked = true;
 }
+
+if (storage.status && statusText !== null) {
+    statusText.innerText = storage.status;
+}
+
+if (enabledSwitch !== null) {
+    enabledSwitch.addEventListener('change', async (event) => {
+        const target = event.currentTarget as HTMLInputElement;
+        await chrome.storage.session.set({enabled: target.checked});
+
+        chrome.runtime.sendMessage({enabled: target.checked});
+    });
+}
+
+chrome.runtime.onMessage.addListener((message : Message, sender, sendResponse) => {
+    if (statusText !== null) {
+        statusText.innerText = message.status;
+    }
+
+    if (enabledSwitch !== null) {
+        enabledSwitch.checked = message.enabled;
+    }
+
+    chrome.storage.session.set({enabled: message.enabled, status: message.status});
+});
